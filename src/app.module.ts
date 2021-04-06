8d5be2ef59e564d3e4e3304150a94d2e9b5e3ce5
Gabriel Scatamburlo <gabriel@multipedidos.com.br>
2021-04-05 21:54:05 -0300

    adicionando logger globalmente na aplicação

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './configs/typeorm.config';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { WinstonModule } from 'nest-winston';
+import { winsonConfig } from './configs/winston-configs';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggerInterceptor } from './interceptors/logger.interceptor';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UserModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig), 
+    WinstonModule.forRoot(winsonConfig),
+    UserModule, 
+    AuthModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor,
+    }
+  ],
 })
 export class AppModule {}
